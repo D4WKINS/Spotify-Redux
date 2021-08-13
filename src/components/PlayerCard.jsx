491fe2d77adef1ace687a8d2f0b6f7311e36ca8b
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-08-13 16:47:14 +0100

    fix

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -13,24 +13,23 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const PlayerCard = (props) => {
-  return (
-    <div
-      className={`player-left d-flex align-items-center ${
-        !props.activeSong && "d-none"
-      }`}>
+  return props.activeSong === undefined ? (
+    <></>
+  ) : (
+    <div className="player-left d-flex align-items-center">
       <img src={props.activeSong?.img} alt="" className="p-0" />
       <div className="d-flex flex-column d-none d-sm-flex">
         <div className="player-song-title text-white">{props.activeSong?.title}</div>
         <div className="player-author">{props.activeSong?.artist.name}</div>
       </div>
-      {props.likes.includes(props.activeSong.id) ? (
+      {props.likes.includes(props.activeSong?.id) ? (
         <i
           className="far fa-heart d-none d-md-flex heart-icon red-heart"
-          onClick={() => props.unlikeSong(props.activeSong.id)}></i>
+          onClick={() => props.unlikeSong(props.activeSong?.id)}></i>
       ) : (
         <i
           className="far fa-heart d-none d-md-flex heart-icon"
-          onClick={() => props.likeSong(props.activeSong.id)}></i>
+          onClick={() => props.likeSong(props.activeSong?.id)}></i>
       )}
       <i className="fas fa-tv d-none d-md-flex"></i>
     </div>
